7083e8bd3b465fc5654563006931f49c2d2b26fc
Naor Malka <malknaor@gmail.com>
2020-02-02 12:33:16 +0200

    Update favorite button css

--- src/App/common/FavoriteButton/FavoriteButton.js
+++ src/App/common/FavoriteButton/FavoriteButton.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Icon } from 'semantic-ui-react';
 
 import localStorageService from '../../../services/localStorageService';
+import './FavoriteButton.scss';
 
 const FavoriteButton = props => {
     const { addToFavorites, removeFavorite, location } = props;
@@ -21,8 +22,7 @@ const FavoriteButton = props => {
         return `fav-icon heart ${localStorageService.isLocationFavorite(location.Key) ? '' : 'outline'} icon`;
     };
 
-    const render = () => <Icon className={getClassName()} onClick={onClick}></Icon>;
-
+    const render = () => <Icon className={`fav-button ${getClassName()}`} onClick={onClick}></Icon>;
 
     return render();
 };
